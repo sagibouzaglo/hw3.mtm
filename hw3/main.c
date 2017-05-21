750f2b6674a909eb0f1f99ab883bb20238305b66
sagibouzaglo <sagibouzaglo@campus.technion.ac.il>
2017-05-21 19:06:11 +0300

    Initial Commit

---
+++ hw3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hw3
+//
+//  Created by sagi bouzaglo on 21/05/2017.
+//  Copyright © 2017 sagi bouzaglo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
