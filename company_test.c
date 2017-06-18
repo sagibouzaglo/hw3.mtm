45c60482af17767af60fa4f709d3d45403dc43de
sagibouzaglo <sagibouzaglo@campus.technion.ac.il>
2017-06-18 22:32:09 +0300

    and

--- company_test.c
+++ company_test.c
@@ -8,8 +8,85 @@
 
 #include "test_utilities.h"
 #include "company.h"
-#include <stdio.h>
-#include <assert.h>
-#include <stdbool.h>
 #include <string.h>
-#include <stdlib.h>
+
+static bool testCompanyCreate(){
+    CompanyReturn Result;
+    ASSERT_TEST(companyCreate(NULL, 0, &Result)==NULL);
+    ASSERT_TEST(Result == COM_NULL_PARAMETER);
+    return true;
+}
+
+static bool testCompanyCopy(){
+    ASSERT_TEST(companyCopy(NULL)==NULL);
+    CompanyReturn Result;
+    char* email = {"test@"} ;
+    Company company = companyCreate(email, 0, &Result);
+    Company company2 = companyCopy(company);
+    ASSERT_TEST(companyCompare(company, company2)==0);
+    companyDestroy(company);
+    companyDestroy(company2);
+    return true;
+}
+
+static bool testCompanyCompare(){
+    CompanyReturn Result;
+    char* email = {"test@"} ;
+    Company company = companyCreate(email, 0, &Result);
+    ASSERT_TEST(Result==COM_SUCCESS);
+    Company company2 = companyCreate(email, 0, &Result);
+    ASSERT_TEST(Result==COM_SUCCESS);
+    ASSERT_TEST(companyCompare(company, company2)==0);
+    companyDestroy(company);
+    companyDestroy(company2);
+    return true;
+}
+
+static bool testEmailCompany(){
+    CompanyReturn Result;
+    char* email = {"test@"} ;
+    Company company = companyCreate(email, 0, &Result);
+    ASSERT_TEST(Result==COM_SUCCESS);
+    ASSERT_TEST(strcmp(getEmailCompany(company),email)==0);
+    companyDestroy(company);
+    return true;
+}
+
+static bool testFacultyOfCompany(){
+    CompanyReturn Result;
+    char* email = {"test@"} ;
+    Company company = companyCreate(email, 0, &Result);
+    ASSERT_TEST(Result==COM_SUCCESS);
+    ASSERT_TEST(getFacultyOfCompany(company)==0);
+    companyDestroy(company);
+    return true;
+}
+
+static bool testGetCompanyRooms(){
+    CompanyReturn Result;
+    char* email = {"test@"} ;
+    Company company = companyCreate(email, 0, &Result);
+    ASSERT_TEST(Result==COM_SUCCESS);
+    ASSERT_TEST(getCompanyRooms(company)!=NULL);
+    companyDestroy(company);
+    return true;
+}
+
+static bool testCompanyDestroy(){
+    CompanyReturn Result;
+    char* email = {"test@"} ;
+    Company company = companyCreate(email, 0, &Result);
+    companyDestroy(company);
+    return true;
+}
+
+int main (int argv, char** arc){
+    RUN_TEST(testCompanyCreate);
+    RUN_TEST(testCompanyCopy);
+    RUN_TEST(testCompanyCompare);
+    RUN_TEST(testEmailCompany);
+    RUN_TEST(testFacultyOfCompany);
+    RUN_TEST(testGetCompanyRooms);
+    RUN_TEST(testCompanyDestroy);
+    return 0;
+}
